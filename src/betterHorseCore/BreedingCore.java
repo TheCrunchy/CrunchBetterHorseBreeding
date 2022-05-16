921c4fe88795eb9acabc7c9b08d8126c85ff786d
TheCrunchy <crunchycamsta@gmail.com>
2022-05-16 22:11:53 +0100

    Added config files

--- src/betterHorseCore/BreedingCore.java
+++ src/betterHorseCore/BreedingCore.java
@@ -4,6 +4,9 @@ import java.text.DecimalFormat;
 
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
@@ -20,15 +23,49 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
 public class BreedingCore extends JavaPlugin {
+	double MaxSpeed, MaxHearts, MaxJump, SpeedPercent, JumpPercent, HeartsPercent;
+	boolean ConsumeBook;
 	@Override
 	public void onEnable() {
 
 		getServer().getPluginManager().registerEvents(new HorseBreed(), this);
-
+		this.getCommand("HorsesReload").setExecutor(new reload());
+		saveDefaultConfig();
+		MaxSpeed = getConfig().getDouble("MaxSpeed");
+		MaxHearts = getConfig().getDouble("MaxHearts");
+		MaxJump = getConfig().getDouble("MaxJump");
+		SpeedPercent = getConfig().getDouble("SpeedPercent");
+		HeartsPercent = getConfig().getDouble("HeartsPercent");
+		JumpPercent = getConfig().getDouble("JumpPercent");
+		ConsumeBook = getConfig().getBoolean("ConsumeBook");
 	}
+	public class reload implements CommandExecutor {
 
+		// This method is called, when somebody uses our command
+		@Override
+		@EventHandler
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				if (player.hasPermission("Horses.Reload")){
+					reloadConfig();
+					MaxSpeed = getConfig().getDouble("MaxSpeed");
+					MaxHearts = getConfig().getDouble("MaxHearts");
+					MaxJump = getConfig().getDouble("MaxJump");
+					SpeedPercent = getConfig().getDouble("SpeedPercent");
+					HeartsPercent = getConfig().getDouble("HeartsPercent");
+					JumpPercent = getConfig().getDouble("JumpPercent");
+					ConsumeBook = getConfig().getBoolean("ConsumeBook");
+					player.sendMessage("Successfully reloaded config");
+				}
+				else {
+					player.sendMessage("Insufficient Permissions");
+				}
+			}
+			return false;
+		}
+	}
 	public class HorseBreed implements Listener {
 	     @EventHandler(priority = EventPriority.LOWEST)
 	        public void onDamage(final EntityDamageByEntityEvent event) {
@@ -75,19 +112,26 @@ public class BreedingCore extends JavaPlugin {
 				}
 				LivingEntity horse = (LivingEntity) event.getRightClicked();
 				double jump = horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH).getBaseValue();
-				if (jump > 0.3) {
-					horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH).setBaseValue(0.3);
+				double health = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+				double speed = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
+
+				if (jump > MaxJump) {
+					horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH).setBaseValue(MaxJump);
+				}
+				if (health > MaxHearts) {
+					horse.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(MaxHearts);
+				}
+				if (speed > MaxSpeed) {
+					horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(MaxSpeed);
 				}
 				if (itemInHand.getType() == Material.BOOK) {
-					
-					double speed = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getValue();
-					double health = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-					
 					DecimalFormat df = new DecimalFormat("#.##");
 					player.sendMessage("Speed: " + df.format(speed));
 					player.sendMessage("Health: " +  df.format(health));
 					player.sendMessage("Jump: " +  df.format(jump));
+					if (ConsumeBook) {
 					itemInHand.setAmount(itemInHand.getAmount() - 1);
+					}
 					event.setCancelled(true);
 				}
 			}
@@ -124,18 +168,18 @@ public class BreedingCore extends JavaPlugin {
 			double avgSpeed = (DadSpeed + MomSpeed) / 2;
 			double avgJump = (DadJump + MomJump) / 2;
 
-			double newHealth = avgHealth * 1.15;
-			double newSpeed = avgSpeed * 1.15;
-			double newJump = avgJump * 1.15;
+			double newHealth = avgHealth * HeartsPercent;
+			double newSpeed = avgSpeed * SpeedPercent;
+			double newJump = avgJump * JumpPercent;
 
-			if (newHealth >= 40) {
-				newHealth = 40;
+			if (newHealth >= MaxHearts) {
+				newHealth = MaxHearts;
 			}
-			if (newSpeed >= 0.35) {
-				newSpeed = 0.35;
+			if (newSpeed >= MaxSpeed) {
+				newSpeed = MaxSpeed;
 			}
-			if (newJump >= 0.3) {
-				newJump = 0.3;
+			if (newJump >= MaxJump) {
+				newJump = MaxJump;
 			}
 
 			child.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(newHealth);
